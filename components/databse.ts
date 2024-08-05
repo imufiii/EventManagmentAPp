c879e4d3bfe41fe90da445e440ca3ffe939c68d0
Mufeed Muneer <117446007+imufiii@users.noreply.github.com>
2024-08-05 04:42:22 -0400

    deleted some console log which i have used for testing the app

--- components/databse.ts
+++ components/databse.ts
@@ -15,7 +15,7 @@ export const loadTasks = async (): Promise<TaskProps[]> => {
     const snapshot = await getDocs(collection(FIRESTORE_DB, 'tasks'));
     return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() as TaskProps }));
   } catch (error) {
-    console.error("Error loading tasks:", error);
+    
     throw error;
   }
 };
@@ -24,10 +24,10 @@ export const loadTasks = async (): Promise<TaskProps[]> => {
 export const addTask = async (task: Omit<TaskProps, 'id'>): Promise<DocumentReference> => {
   try {
     const docRef = await addDoc(collection(FIRESTORE_DB, 'tasks'), task);
-    console.log("Task added successfully.");
+    
     return docRef;
   } catch (error) {
-    console.error("Error adding task:", error);
+   
     throw error;
   }
 };
@@ -37,9 +37,9 @@ export const updateTask = async (task: TaskProps): Promise<void> => {
   try {
     const taskRef = doc(FIRESTORE_DB, 'tasks', task.id);
     await updateDoc(taskRef, { done: task.done });
-    console.log("Task updated successfully.");
+   
   } catch (error) {
-    console.error("Error updating task:", error);
+    
     throw error;
   }
 };
@@ -49,9 +49,9 @@ export const removeTask = async (taskId: string): Promise<void> => {
   try {
     const taskRef = doc(FIRESTORE_DB, 'tasks', taskId);
     await deleteDoc(taskRef);
-    console.log("Task removed with ID:", taskId);
+    
   } catch (error) {
-    console.error("Error removing task:", error);
+    
     throw error;
   }
 };
