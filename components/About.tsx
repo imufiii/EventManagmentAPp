f7e6a9b04ad27e4f7c0be81c4b9d4f6f42276253
ArulSajeev <157263914+ArulSajeev@users.noreply.github.com>
2024-08-05 22:22:19 -0400

    a
    a

--- components/About.tsx
+++ components/About.tsx
@@ -1,5 +1,16 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+  Image,
+  ScrollView,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { signOut } from "firebase/auth";
+import { AUTH } from "../firebaseConfig";
 
 const About: React.FC = () => {
   return (
